1508dc366807868895c6573a3d24f42b880dce8e
jakatsa <jakatsa5@gmail.com>
2025-06-26 21:13:48 +0300

    web socket implementation

--- chatsystemproj/chatsystemproj/asgi.py
+++ chatsystemproj/chatsystemproj/asgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+from channels.routing import ProtocolTypeRouter,URLRouter
+from channels.auth import AuthMiddlewareStack
+from chatapp.routing import websocket_urlpatterns
 
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatsystemproj.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
+})
